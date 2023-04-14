df269da87754ddef89be31a563c7a4b9398f9e72
devolo13 <devolo13@gmail.com>
2023-04-14 15:59:28 -0500

    initial commit

---
+++ flask_app/controllers/users_controller.py
@@ -0,0 +1,60 @@
+from flask_app import app
+from flask import render_template, redirect, request
+from flask_app.models.user_model import User
+
+# default page for users who aren't logged in
+@app.route('/')
+def login_page():
+    # if user already logged in, reroute to their page
+    # else render index.html
+    return render_template('login.html')
+
+
+# route for verifying user inputs, adding them to the db, then redirecting to ...?
+@app.route('/register_user')
+def register_new_user():
+    # format user inputs into the standard format
+    data = {
+        **request.form
+    }
+    # what does our data look like? we should only get the registration data, not the login data
+    print(data)
+    # test if the users inputs were valid
+    if User.validate_registration():
+        # if user input was valid, add them to the db and redirect to their page
+        user = User.add_new_user()
+        redirect('single_user.html', user=user)
+    else:
+        # if user input was not validate, show errors and redirect back to registration/login page
+        redirect('/')
+
+# route for logging in existing users and redirecting to their page
+@app.route('/login_user')
+def login_user():
+    # format user inputs into the standard format
+    data = {
+        **request.form
+    }
+    # see what data we got back. we only want the login data, not the registration data
+    print(data)
+    # see if our user exists in our database
+    if data.email in User.get_all_emails:
+        print(f"the email address {data.email} is in our database")
+        # see if the password matches the existing users password
+            # store the user's information in session
+            # redirect them to their individual page
+    else:
+        print(F"the email address {data.email} is not in our database")
+        # show the error and redirect to ('/')
+
+# page for an individual user
+@app.route('/users/<int:id>')
+def logged_in_user_page():
+    # may need additional logic
+    return render_template('single_user.html')
+
+# route for logging out a user and redirecting to login page
+@app.route('/logout')
+def log_out_a_user():
+    # clear out the session
+    return redirect('/')
---
+++ flask_app/models/user_model.py
@@ -0,0 +1,58 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask_app import DATABASE
+import re
+
+class User:
+    def __init__(self, data):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.password = data['password']
+
+    # method for getting all users. returns an array of user objects
+    @classmethod
+    def get_all(cls):
+        query = 'SELECT * FROM users;'
+        results = connectToMySQL(DATABASE).query_db(query)
+        users = []
+        for user in results:
+            users.append(cls(user))
+        return users
+    
+    # method for getting all user emails. returns an array of email addresses
+    @classmethod
+    def get_all_emails(cls):
+        query = "SELECT email FROM users;"
+        results = connectToMySQL(DATABASE).query_db(query)
+        # is this emails array necessary? do we get an array of emails back from the db?
+        emails = []
+        for email in results:
+            emails.append(email)
+
+    # method for validating user registration inputs
+    @staticmethod
+    def validate_registration_inputs(cls, data):
+        valid_inputs = True
+        # validate first name length
+        if (len(data.first_name) < 2):
+            # create an error flag for first_name length
+            valid_inputs = False
+        # validate last name length
+        if (len(data.last_name) < 2):
+            # create an error flag for last_name length
+            valid_inputs = False
+        # validate email format
+        if not re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$').match(user['email']):
+            # create an error flag for email
+            valid_inputs = False
+        # validate password length
+        if (len(data.password) < 8):
+            # create an error flag for password length
+            valid_inputs = False
+        # validate password confirmation
+        if data.password != data.confirm_password:
+            # create an error flag for password matching
+            valid_inputs = False
+        return valid_inputs
+    # NEEDS MORE CLASS METHODS AND A STATIC METHOD FOR VERIFYING REGISTRATION INPUTS
